bc0f8cb9fc0d8148f02ada460bc4fa8ce1a7d6cb
hoonti06 <hoonti06@gmail.com>
2024-05-18 15:58:51 +0900

    first commit

---
+++ src/main/java/me/hoonti06/workingeffectivelywithlegacycode/WorkingEffectivelyWithLegacyCodeApplication.java
@@ -0,0 +1,13 @@
+package me.hoonti06.workingeffectivelywithlegacycode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkingEffectivelyWithLegacyCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkingEffectivelyWithLegacyCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/hoonti06/workingeffectivelywithlegacycode/WorkingEffectivelyWithLegacyCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hoonti06.workingeffectivelywithlegacycode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkingEffectivelyWithLegacyCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
